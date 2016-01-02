07dfe0f06cdf3f40819b34d9fd4711f4edf4a753
talseb1 <talseb0@hotmail.com>
2016-01-02 18:00:32 +0100

    Envois fichier source

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <ul id="user-menu">
+          <li><a href="#">Sign Up</a></li>
+          <li><a href="#">My account</a></li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
